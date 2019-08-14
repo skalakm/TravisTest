e2b2f1192ed962025097c3daeb26af00c70ec7b6
Michael Skalak <skalakm@dickinson.edu>
2019-08-14 13:01:13 -0400

    change build but didnt fix anything

--- src/thePackage/TestClassTest.java
+++ src/thePackage/TestClassTest.java
@@ -1,18 +1,20 @@
 package thePackage;
 
 import org.junit.Test;
-
+import org.junit.*;
 import static org.junit.Assert.fail;
 
 public class TestClassTest {
 
+
+
+
     @Test
-    public void testNothing() {
-    }
+    public void testTest(){}
 
     @Test
     public void testWillAlwaysFail() {
-        fail("An error message");
+	fail("An error message");
     }
 
 }
